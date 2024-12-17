e9691d9cd406bf42e251651e5cc1e9fa2d6091cd
Hosen Nes <husen43nas4@gmail.com>
2024-12-17 21:35:30 +0200

    alo

---
+++ app/src/main/java/com/example/pro55/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.pro55;
+
+import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private ImageView imageView;
+    private ImageButton nextButton, prevButton;
+
+
+    private int[] images = {
+            R.drawable.im1,
+            R.drawable.im2,
+            R.drawable.im3,
+            R.drawable.im4
+    };
+
+    private int currentIndex = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
+        nextButton = findViewById(R.id.nextButton);
+        prevButton = findViewById(R.id.prevButton);
+
+
+        nextButton.setOnClickListener(v -> {
+            currentIndex = (currentIndex + 1) % images.length;
+            imageView.setImageResource(images[currentIndex]);
+        });
+
+
+        prevButton.setOnClickListener(v -> {
+            currentIndex = (currentIndex - 1 + images.length) % images.length;
+            imageView.setImageResource(images[currentIndex]);
+
+  });
+}}
